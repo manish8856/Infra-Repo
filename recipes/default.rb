55dafdec91b93846b51764e8c6a84c7be89a6d17
manish8856 <manish.kumar@crichq.com>
2016-08-23 12:06:57 +0530

    Unicorn Install

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,4 +7,10 @@ end
 package 'git' do
   action :/tmp/install
 end
+ruby_block "ensure only our unicorn version is installed by deinstalling any other version" do
+  block do
+    ensure_only_gem_version("rack", node[:unicorn][:rack_version])
+    ensure_only_gem_version("unicorn", node[:unicorn][:version])
+  end
+end
 
