3468236fadfa23956a54a436c7d8f54c1e751d67
manish8856 <manish.kumar@crichq.com>
2016-08-24 10:12:37 +0530

    Delete Unicorn code and Install Postgress

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,36 @@ end
 package 'git' do
   action :/tmp/install
 end
-ruby_block "ensure only our unicorn version is installed by deinstalling any other version" do
-  block do
-    ensure_only_gem_version("rack", node[:unicorn][:rack_version])
-    ensure_only_gem_version("unicorn", node[:unicorn][:version])
+if node[:opsworks_postgresql] && ([:devel_package, :client_package].all? {|s| node[:opsworks_postgresql].key? s})
+  # Only use the default package names if attributes exist and they are
+  # defined so we don't break anyone who has overriden this recipe
+  [node[:opsworks_postgresql][:devel_package], node[:opsworks_postgresql][:client_package]].each do |pkg|
+    package pkg do
+      action :install
+      retries 3
+      retry_delay 5
+    end
+  end
+else
+  # old behavior for backwards compatibility
+  package "postgresql-devel" do
+    package_name value_for_platform(
+      ["centos","redhat","fedora","amazon"] => {"default" => "postgresql-devel"},
+      "ubuntu" => {"default" => "libpq-dev"}
+    )
+    action :install
+    retries 3
+    retry_delay 5
+  end
+
+  package "postgresql-client" do
+    package_name value_for_platform(
+      ["centos","redhat","fedora","amazon"] => {"default" => "postgresql"},
+      "default" => "postgresql-client"
+    )
+    action :install
+    retries 3
+    retry_delay 5
   end
 end
 
